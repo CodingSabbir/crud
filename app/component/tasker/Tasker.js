1e20128ba30ed7f50a9b321e9f9f8e8d5ffc214c
SabbirPnH <mdsabbirrahmanm740@gmail.com>
2024-09-23 05:10:55 -1200

    tasker

---
+++ app/component/tasker/Tasker.js
@@ -0,0 +1,79 @@
+import AddTaskButtonSection from "../AddTaskButtonSection"
+import { FiHeart } from 'react-icons/fi';
+
+
+
+export default function Tasker(){
+    const tableItems = [
+        {
+            name: "Liam James",
+            email: "liamjames@example.com",
+            position: "Software engineer",
+            salary: "$100K"
+        },
+        {
+            name: "Olivia Emma",
+            email: "oliviaemma@example.com",
+            position: "Product designer",
+            salary: "$90K"
+        },
+        {
+            name: "William Benjamin",
+            email: "william.benjamin@example.com",
+            position: "Front-end developer",
+            salary: "$80K"
+        },
+        {
+            name: "Henry Theodore",
+            email: "henrytheodore@example.com",
+            position: "Laravel engineer",
+            salary: "$120K"
+        },
+        {
+            name: "Amelia Elijah",
+            email: "amelia.elijah@example.com",
+            position: "Open source manager",
+            salary: "$75K"
+        },
+    ]
+
+    return (
+        <div className="max-w-screen-xl mx-auto px-4 md:px-8 py-10">
+           <AddTaskButtonSection/>
+            <div className="mt-12 shadow-sm border rounded-lg overflow-x-auto">
+                <table className="w-full table-auto text-sm text-left">
+                    <thead className="bg-gray-50 text-gray-600 font-medium border-b">
+                        <tr>
+                            <th className="py-3 px-6"> Username</th>
+                            <th className="py-3 px-6">Email</th>
+                            <th className="py-3 px-6">Position</th>
+                            <th className="py-3 px-6">Salary</th>
+                            <th className="py-3 px-6"></th>
+
+                        </tr>
+                    </thead>
+                    <tbody className="text-gray-600 divide-y">
+                        {
+                            tableItems.map((item, idx) => (
+                                <tr key={idx}>
+                                    <td className="px-6 py-4 whitespace-nowrap flex gap-2 items-center"> <FiHeart className="text-red-500 cursor-pointer" /> {item.name}</td>
+                                    <td className="px-6 py-4 whitespace-nowrap">{item.email}</td>
+                                    <td className="px-6 py-4 whitespace-nowrap">{item.position}</td>
+                                    <td className="px-6 py-4 whitespace-nowrap">{item.salary}</td>
+                                    <td className="text-right px-6 whitespace-nowrap">
+                                        <a href="#" className="py-2 px-3 font-medium text-indigo-600 hover:text-indigo-500 duration-150 hover:bg-gray-50 rounded-lg">
+                                            Edit
+                                        </a>
+                                        <button href="#" className="py-2 leading-none px-3 font-medium text-red-600 hover:text-red-500 duration-150 hover:bg-gray-50 rounded-lg">
+                                            Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+};
